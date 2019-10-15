15b041adeac8a019419b1106184257fd7503841a
blue0101 <skqksh@gmail.com>
2019-10-15 10:34:19 +0900

    install and init react-router-dom

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
+const Root = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Root />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
